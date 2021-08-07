0f0e4aefc1b8e9653e12d0ee81c25408cd312e05
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-08-06 20:23:22 -0700

    add license and statement that modifications were made

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
+// Modifications by:
+//  - Adrian Edwards
 
 //! Packet Definition for SpacePacket
 
